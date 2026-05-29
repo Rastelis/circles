2c96f0f139c0d12be2a9c0d015a3408cea92360f
LaimisKirsa <laimisa@gmail.com>
2026-05-29 14:25:00 +0300

    config variables created

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
-import { Chain } from './parts/chain';
+import { Chain, type ChainConfig } from './parts/chain';
 import { Renderer } from './render/Renderer';
 import './style.scss';
 
 const renderer = new Renderer(1000, 800);
+const length = [200, 200, 150];
+const velocity = [0.01, 0.01 * Math.sqrt(2), 0.05, 0.07, 0.09];
 
-const chain = new Chain(5, renderer.centerVector, {
+const chainConfig = {
   dots: {
-    trail: 'all',
+    draw: 'all',
+    trail: 'last',
   },
-});
+  lines: {
+    velocity: velocity,
+    length: length,
+  },
+} as Partial<ChainConfig>;
+
+const chain = new Chain(2, renderer.centerVector, chainConfig);
 
 (
   window as typeof window & {
