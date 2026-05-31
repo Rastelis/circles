bf4bcdbf30ff80592a19508cd807e885e3637231
LaimisKirsa <laimisa@gmail.com>
2026-05-31 14:44:12 +0300

    added sim speed regulation

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,27 @@
 import { Chain, type ChainConfig } from './parts/chain';
+import type { TrailConfig } from './parts/dot';
 import { Renderer } from './render/Renderer';
 import './style.scss';
 
 const renderer = new Renderer(1000, 800);
-const length = [200, 50, 150];
-const velocity = [0.01, 0.051, 0.051, 0.09];
+const length = [50, 100, 150];
+const velocity = [0.00001, 0.000051, 0.000031, 0.09];
 
 const chainConfig = {
   dots: {
     draw: 'all',
     trail: 'all',
+    // trailConfig: [
+    //   {
+    //     color: 'lime',
+    //   },
+    //   {
+    //     color: 'yellow',
+    //   },
+    //   {
+    //     color: 'red',
+    //   },
+    // ] as Partial<TrailConfig>,
   },
   lines: {
     velocity: velocity,
@@ -34,9 +46,12 @@ draw();
 
 function draw() {
   renderer.clear();
-
-  chain.lines.forEach((line) => line.update());
-  chain.dots.forEach((dot) => dot.update());
+  let simsteps = 5000;
+  while (simsteps > 0) {
+    simsteps--;
+    chain.lines.forEach((line) => line.update());
+    chain.dots.forEach((dot) => dot.update());
+  }
 
   chain.dots.forEach((dot) => renderer.drawDot(dot));
   chain.lines.forEach((line) => renderer.drawLine(line));
